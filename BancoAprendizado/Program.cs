66c23fcff0fb67921d029f74af237d511819b181
nataliarissi <nataliarissirocha@gmail.com>
2023-09-20 01:19:10 -0300

    Continuidade do estudos

--- BancoAprendizado/Program.cs
+++ BancoAprendizado/Program.cs
@@ -42,14 +42,69 @@
 
 ///////////////////////////////////////////////////////
 
-// ternária
-// (condição) ? valor_se_verdadeiro : valor_se_falso
+// // ternária
+// // (condição) ? valor_se_verdadeiro : valor_se_falso
 
-double preco = double.Parse(Console.ReadLine());
-double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
-    // if(preco < 20.0)
-    //     desconto = preco * 0.1;
-    // else 
-    //     desconto = preco * 0.05;
+// double preco = double.Parse(Console.ReadLine());
+// double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+//     // if(preco < 20.0)
+//     //     desconto = preco * 0.1;
+//     // else 
+//     //     desconto = preco * 0.05;
 
-Console.WriteLine(desconto);
+// Console.WriteLine(desconto);
+
+///////////////////////////////////////////////////////
+
+using System.Data.SqlTypes;
+
+// string original = "abcde FGHIJ ABC abc DEFG     ";
+// //string é uma varável imutável, não altera contéudo
+// string s1 = original.ToUpper();
+// string s2 = original.ToLower();
+// string s3 = original.Trim();
+
+// int n1 = original.IndexOf("bc"); //procura
+// int n2 = original.LastIndexOf("bc");
+
+// string s4 = original.Substring(3);
+// string s5 = original.Substring(3, 5);
+
+// string s6 = original.Replace('a', 'x');
+// string s7 = original.Replace("abc", "xy");
+
+// bool b1 = String.IsNullOrEmpty(original);
+// bool b2 = String.IsNullOrWhiteSpace(original);
+
+// Console.WriteLine("Original: " + original);
+// Console.WriteLine("ToUpper: " + s1);
+// Console.WriteLine("ToLower: " + s2);
+// Console.WriteLine("Trim: " + s3); //apaga espaço em branco
+// Console.WriteLine("IndexOf('bc'): " + n1); //aparece na primeira posição
+// Console.WriteLine("LastIndexOf('bc'): " + n2); //última ocorrência do bc
+// Console.WriteLine("Substring(3): " + s4); //recortar string
+// Console.WriteLine("Substring(3, 5): " + s5); //imprimiu 5 caracteres a partir da posição 3
+// Console.WriteLine("Replace('a', 'x'): " + s6); //substituir parte da string por outra
+// Console.WriteLine("Replace('abc', 'xy'): " + s7);
+// Console.WriteLine("IsNullOrEmpty: " + b1);
+// Console.WriteLine("IsNullOrWhiteSpace: " + b2); //para conferir se são espaços em branco
+
+///////////////////////////////////////////////////////
+
+//armazenamento de data e horário
+// DateTime d1 = DateTime.Now;
+
+// Console.WriteLine(d1);
+// Console.WriteLine(d1.Ticks);
+
+DateTime d1 = new DateTime(2023, 09, 19);
+Console.WriteLine(d1);
+
+//especificar horário
+DateTime d2 = new DateTime(2018, 09, 19, 23, 40, 7);
+Console.WriteLine(d2);
+
+DateTime d3 = new DateTime(2018, 09, 19, 23, 40, 7, 500);
+Console.WriteLine(d3);
+
+//horário global ou local
